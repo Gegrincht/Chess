54a7b67118528c7cef1099c07a4a9c88b4958dda
Sebastian Schuhmacher <85761348+Gegrincht@users.noreply.github.com>
2026-03-29 19:04:51 +0200

    290320261904

--- GUI/src/Main.cpp
+++ GUI/src/Main.cpp
@@ -2,6 +2,25 @@
 #include "Logger.h"
 #include <iostream>
 #include <chrono>
+#include <string>
+#include <random>
+
+std::string randomString(int length) {
+    const std::string chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "0123456789";
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(0, chars.size() - 1);
+
+    std::string result;
+    for (int i = 0; i < length; ++i) {
+        result += chars[dist(gen)];
+    }
+
+    return result;
+}
 
 void printBoard(ChessGame& game) {
     std::cout << "     A B C D E F G H\n  |-------------------|\n";
@@ -16,7 +35,7 @@ void printBoard(ChessGame& game) {
     std::cout << "  |-------------------|\n     A B C D E F G H\n";
 }
 
-void benchmark(long int ITERATIONS) {
+void benchmark(long int iterations = 10000) {
     long long total = 0;
     long long totalForPercent = 0;
     std::cout << "Press Enter to start test...";
@@ -31,7 +50,7 @@ void benchmark(long int ITERATIONS) {
     
     std::cout << "Starte Benchmark... \n";
     int lastPercent = 0;
-    for (int i = 0; i < ITERATIONS; ++i) {
+    for (int i = 0; i < iterations; ++i) {
         ChessGame tgame;
 
         auto start = std::chrono::steady_clock::now();
@@ -44,9 +63,9 @@ void benchmark(long int ITERATIONS) {
         total += tempTime;
         totalForPercent += tempTime;
 
-        int percent = (i * 100) / ITERATIONS;
+        int percent = (i * 100) / iterations;
         if (percent > lastPercent) {
-            int iterationsInPercent = i - (lastPercent * ITERATIONS / 100);
+            int iterationsInPercent = i - (lastPercent * iterations / 100);
 
             std::cout << "\033[2K\r" << std::chrono::system_clock::now() << " |---| " << percent << "%\n";
             std::cout << "\033[2K\rProgress\t: " << i << "\n";
@@ -59,7 +78,7 @@ void benchmark(long int ITERATIONS) {
         }
     }
     std::cout << "\033[2J\033[H" << std::flush;
-    std::cout << "Average: " << (total / ITERATIONS) << "us\n";
+    std::cout << "Average: " << (total / iterations) << "us\n";
     std::cout << "Total: " << (total / 1000.0) << "ms\n";
     std::cout << "Total Seconds: " << (total / 1000000.0) << "s\n";
 
@@ -67,9 +86,21 @@ void benchmark(long int ITERATIONS) {
 }
 
 int main() {
-	std::cout << "Test" << std::endl;
+    std::cout << "Test" << std::endl;
 
-	ChessGame game;
+    ChessGame game;
 
     printBoard(game);
+    std::string randStr = randomString(6);
+    std::cout << "Do you want to do a benchmark? If so type '" << randStr << "'. If the copied text is not exactly the same, program will close.\nPLEASE TYPE HERE: ";
+    std::string line;
+    std::getline(std::cin, line);
+
+    std::istringstream iss(line);
+
+    std::string code;
+    int number = 10000;
+
+    iss >> code >> number;
+    if (code == randStr) benchmark(number);
 }
